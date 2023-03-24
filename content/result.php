731a93dd47118a53764bbf5d6378adca8f101e95
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2023-03-25 00:18:31 +0900

    合計点数表示

--- content/result.php
+++ content/result.php
@@ -2,52 +2,68 @@
 error_reporting(0);
 require '../dbconnect.php';
 session_start();
-$select_sql = 'select DISTINCT task_id,user_id,answer_count from answer 
+$ans_select = 'select DISTINCT task_id,user_id,answer_count from answer 
 where task_id = ? and user_id = ?';
-$stmt = $pdo->prepare($select_sql);
+$ans_stmt = $pdo->prepare($ans_select);
 
-$stmt->execute(array($_POST['task_id'],$_SESSION['id']));
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$ans_stmt->execute(array($_POST['task_id'],$_SESSION['id']));
+$flag_result = $ans_stmt->fetch(PDO::FETCH_ASSOC);
 
 $flag = 'insert';
- if(!empty($result['user_id'])){
+ if(!empty($flag_result['user_id'])){
     $flag = 'update';
  }
 
- $answer_count = $result['answer_count'];
+ $answer_count = $flag_result['answer_count'];
 
 $select_sql = 'select * from question WHERE TaskId = ?';
 $stmt = $pdo->prepare($select_sql);
 $stmt->execute(array($_POST['task_id']));
 
-$insert_sql = 'insert into answer(task_id,number,user_anwser,user_id,answer_count) VALUES';
+$insert_sql = 'insert into answer(task_id,number,user_anwser,
+user_id,answer_count,point) VALUES';
 $insert_args = [];
 
-$update_sql = 'update `answer` SET `user_anwser` = case `number`';
-$update_args = [];
-
+$ans_update = 'update `answer` SET `user_anwser` = case `number`';
+$point_update .= ' point = case number';
+$ans_args = [];
+$point_args = [];
 $question_num = 1;
+
+$max_point = 0;
+$total_point = 0;
 while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+  $point = 0;
   $number = $result['number'];
   $description = $result['sentence'];
+  $max_point += $result['point'];
+
   echo $question_num.".";
-  if($flag == "insert"){
-    $args = [$_POST['task_id'],$number,$_POST['question'.$number],$_SESSION['id'],1];
-    $insert_args = array_merge($insert_args,$args);
-    $insert_sql .= '(?,?,?,?,?),';
-  }else if($flag == "update"){
-    $update_sql .= 'when ? then ? ';
-    $args = [$number,$_POST['question'.$number]];
-    $update_args = array_merge($update_args,$args);
-  }
 
   if($_POST['question'.$number] == $result['answer']){
     print('〇');
+    $point = $result['point'];
+    $total_point += $point;
   }else{
     print('✕');
   }
   print('<hr>');
   print('<br>');
+
+  if($flag == "insert"){
+    $args = [$_POST['task_id'],$number,$_POST['question'.$number],$_SESSION['id'],1,$point];
+    $insert_args = array_merge($insert_args,$args);
+    $insert_sql .= '(?,?,?,?,?,?),';
+  }else if($flag == "update"){
+    $ans_update .= ' when ? then ? ';
+    $point_update .= " when ? then ? ";
+
+    $args = [$number,$_POST['question'.$number]];
+    $ans_args = array_merge($ans_args,$args);
+    $args = [$number,$point];
+    
+    $point_args = array_merge($point_args,$args);
+  }
   $question_num++;
 }
 
@@ -57,12 +73,16 @@ if($flag == 'insert'){
   $stmt = $pdo->prepare($insert_sql);
   $stmt->execute($insert_args);
 }else if($flag == 'update'){
-  $update_sql .= 'end where task_id = ? and user_id = ?';
+  $ans_update .= 'end, '.$point_update.' end where task_id = ? and user_id = ?';
+  $ans_args = array_merge($ans_args,$point_args);
+
   $args = [$_POST['task_id'],$_SESSION['id']];
-  $update_args = array_merge($update_args,$args);
-  $stmt = $pdo->prepare($update_sql);
-  $stmt->execute($update_args);
+  $ans_args = array_merge($ans_args,$args);
+  
+  $stmt = $pdo->prepare($ans_update);
+  $stmt->execute($ans_args);
 }
+echo $max_point."/".$total_point."点";
 ?>
 <button id = "top_button">トップメニューへ戻る</button>
 <script>
